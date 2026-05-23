48bf9295a240a6ac3d7157a4131851559f094704
Jen Anderson <andjenm@gmail.com>
2026-05-24 08:35:21 +1200

    Thought about holidays and reserve queues, updated models)

--- models/loan.ts
+++ models/loan.ts
@@ -1,8 +1,10 @@
-export interface Loans {
-  id: Number
-  book_id: Number
-  lender_id: Number
-  borrower_id: Number
-  status: String // Active || Completed || Overdue
-  due_date: String
+export interface Loan {
+  id: string
+  book_id: string
+  lender_id: string
+  borrower_id: string
+  status: 'Pending' | 'Active' | 'Completed' | 'Overdue' | 'Declined' | 'Cancelled' //Disable requests if on hold, otherwise not available. Can't deal with queues.
+  due_date: string
+  created_at: string
+  updated_at: string
 }
--- models/user.ts
+++ models/user.ts
@@ -1,10 +1,15 @@
 export interface User {
-  id: Number
-  user_name: String
-  email: String
-  postcode: Number
-  status: String //Active, Banned, On Hiatus, Deleted (make placeholder name and deets)
-  borrowed?: Number //Opt, number of books borrowed
-  loaned?: Number //Opt, number of books lent
-  rating?: Number //Ratings
+  id: string
+  user_name: string
+  email: string
+  postcode: number
+  interests?: string[]
+  status: 'Active' | 'Banned' | 'On Hiatus' | 'Deleted'
+  //User manual toggle active, on hiatus on profile to cover holiday/unavailability. Use this to make their collection hidden
+  //Deleted - make placeholder name and deets, retain archive
+  borrowed_count?: number //Opt, number of books borrowed
+  loaned_count?: number //Opt, number of books lent
+  rating?: number //Ratings
+  created_at: string
+  updated_at: string
 }
